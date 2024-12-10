f20d383529a629472b818a1ade04f922aae582f0
Alex-Hua <mr.alex.hua@outlook.com>
2024-12-10 10:05:27 +0800

    v0.0.1 首次移植官方显示驱动,完成3.5in驱动 //TODO 修改项目结构, 适配CubeMX生成的工程

--- Core/Src/fsmc.c
+++ Core/Src/fsmc.c
@@ -171,7 +171,26 @@ static void HAL_FSMC_MspInit(void){
 
 void HAL_SRAM_MspInit(SRAM_HandleTypeDef* sramHandle){
   /* USER CODE BEGIN SRAM_MspInit 0 */
+  GPIO_InitTypeDef gpio_init_struct;
 
+  __HAL_RCC_FSMC_CLK_ENABLE();            /* 使能FSMC时钟 */
+  __HAL_RCC_GPIOD_CLK_ENABLE();           /* 使能GPIOD时钟 */
+  __HAL_RCC_GPIOE_CLK_ENABLE();           /* 使能GPIOE时钟 */
+
+  /* 初始化PD0,1, 8,9,10,14,15 */
+  gpio_init_struct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_8 \
+                         | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_14 | GPIO_PIN_15;
+  gpio_init_struct.Mode = GPIO_MODE_AF_PP;            /* 推挽复用 */
+  gpio_init_struct.Pull = GPIO_PULLUP;                /* 上拉 */
+  gpio_init_struct.Speed = GPIO_SPEED_FREQ_HIGH;      /* 高速 */
+  gpio_init_struct.Alternate = GPIO_AF12_FSMC;        /* 复用为FSMC */
+
+  HAL_GPIO_Init(GPIOD, &gpio_init_struct);            /* 初始化 */
+
+  /* 初始化PE7,8,9,10,11,12,13,14,15 */
+  gpio_init_struct.Pin = GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 \
+                         | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+  HAL_GPIO_Init(GPIOE, &gpio_init_struct);
   /* USER CODE END SRAM_MspInit 0 */
   HAL_FSMC_MspInit();
   /* USER CODE BEGIN SRAM_MspInit 1 */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,13 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdio.h>
 
+#include "sys.h"
+#include "usart.h"
+#include "delay.h"
+#include "led.h"
+#include "lcd.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -93,6 +99,17 @@ int main(void)
   MX_USART1_UART_Init();
   MX_FSMC_Init();
   /* USER CODE BEGIN 2 */
+  uint8_t x = 0;
+  uint8_t lcd_id[12];
+
+  HAL_Init();                             /* 初始化HAL库 */
+  sys_stm32_clock_init(336, 8, 2, 7);     /* 设置时钟,168Mhz */
+  delay_init(168);                        /* 延时初始化 */
+  usart_init(115200);                     /* 串口初始化为115200 */
+  led_init();                             /* 初始化LED */
+  lcd_init();                             /* 初始化LCD */
+  g_point_color = RED;
+  sprintf((char *)lcd_id, "LCD ID:%04X", lcddev.id);  /* 将LCD ID打印到lcd_id数组 */
 
   /* USER CODE END 2 */
 
@@ -100,6 +117,68 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    switch (x)
+    {
+    case 0:
+      lcd_clear(WHITE);
+      break;
+
+    case 1:
+      lcd_clear(BLACK);
+      break;
+
+    case 2:
+      lcd_clear(BLUE);
+      break;
+
+    case 3:
+      lcd_clear(RED);
+      break;
+
+    case 4:
+      lcd_clear(MAGENTA);
+      break;
+
+    case 5:
+      lcd_clear(GREEN);
+      break;
+
+    case 6:
+      lcd_clear(CYAN);
+      break;
+
+    case 7:
+      lcd_clear(YELLOW);
+      break;
+
+    case 8:
+      lcd_clear(BRRED);
+      break;
+
+    case 9:
+      lcd_clear(GRAY);
+      break;
+
+    case 10:
+      lcd_clear(LGRAY);
+      break;
+
+    case 11:
+      lcd_clear(BROWN);
+      break;
+    }
+
+    lcd_show_string(10, 40, 240, 32, 32, "STM32", RED);
+    lcd_show_string(10, 80, 240, 24, 24, "TFTLCD TEST", RED);
+    lcd_show_string(10, 110, 240, 16, 16, "ATOM@ALIENTEK", RED);
+    lcd_show_string(10, 130, 240, 16, 16, (char *)lcd_id, RED); /* 显示LCD ID */
+    x++;
+
+    if (x == 12)
+      x = 0;
+
+    LED0_TOGGLE(); /*红灯闪烁*/
+    delay_ms(1000);
     HAL_GPIO_TogglePin(LED0_GPIO_Port,LED0_Pin);
     HAL_Delay(100);
 
